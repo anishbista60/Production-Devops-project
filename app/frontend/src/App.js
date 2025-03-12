4d1f334e4dc90a5e027e3ebbfbb8ed1ddd77466a
utsab818 <utsabsapkota4231@gmail.com>
2025-03-12 14:59:34 +0545

    env

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
 
-  const API_URL = window._env_.REACT_APP_API_URL || 'http://localhost:8080';
+  const API_URL = window._env_.REACT_APP_API_URL;
   console.log("Using API URL:", API_URL);
   
   const [todos, setTodos] = useState([]);
