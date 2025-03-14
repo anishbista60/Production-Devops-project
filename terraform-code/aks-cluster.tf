940b47b329e786b8c19c616296942001a08f7bbd
Anish Bista <anishbista053@gmail.com>
2025-03-14 13:51:19 +0545

    chore: some changes
    Signed-off-by: Anish Bista <anishbista053@gmail.com>

--- terraform-code/aks-cluster.tf
+++ terraform-code/aks-cluster.tf
@@ -36,9 +36,10 @@ data "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_role_assignment" "aks_acr_pull" {
-  scope                = data.azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.default.identity.0.principal_id
+  principal_id                     = azurerm_kubernetes_cluster.default.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = data.azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
 }
 
 output "aks_principal_id" {
